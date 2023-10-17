0fe5ce2aa45bd7cb744867d3937636f0061b7530
forkgrip <hakusuroku@gmail.com>
2023-10-17 18:18:25 +0900

    ボタンを試験的に追加

--- Etangoapp/Etangoapp/Form1.Designer.cs
+++ Etangoapp/Etangoapp/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.testandanswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 109);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ボタン";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.testbBottonClicked);
+            // 
+            // testandanswer
+            // 
+            this.testandanswer.AutoSize = true;
+            this.testandanswer.Location = new System.Drawing.Point(367, 384);
+            this.testandanswer.Name = "testandanswer";
+            this.testandanswer.Size = new System.Drawing.Size(37, 18);
+            this.testandanswer.TabIndex = 1;
+            this.testandanswer.Text = "test";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.testandanswer);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label testandanswer;
     }
 }
 
--- Etangoapp/Etangoapp/Form1.cs
+++ Etangoapp/Etangoapp/Form1.cs
@@ -16,5 +16,15 @@ namespace Etangoapp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void testbBottonClicked(object sender, EventArgs e)
+        {
+            this.testandanswer.Text = "Answerd";
+        }
     }
 }
